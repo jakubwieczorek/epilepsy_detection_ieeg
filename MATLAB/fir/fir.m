94bcc416e029be6cee2dc8a9b7e4dc17f476470b
jakubwieczorek <jakub.wieczorek0101@gmail.com>
2020-05-30 23:09:31 +0200

    4 hidden + fir

--- MATLAB/fir/fir.m
+++ MATLAB/fir/fir.m
@@ -1,67 +1,58 @@
 clear all;
-time='18'; % hours
-patient='08';
-seizure_number=1;
+time='230'; % hours
+patient='02';
+electrode_number=8;
 
 exp_name =strcat('ID', patient, '_', time, 'h');
 title_name =strcat('ID', patient, '\_', time, 'h');
-load(strcat('../data/ID', patient, '/', exp_name, '.mat'));
-load(strcat('../data/ID', patient, '/ID', patient, '_info.mat'));
+load(strcat('../../data/ID', patient, '/', exp_name, '.mat'));
+load(strcat('../../data/ID', patient, '/ID', patient, '_info.mat'));
 
 fs=double(fs);
 
-y = EEG(1, :);                           % get the first channel
+y = EEG(electrode_number, :);                           % get the first channel
 N = length(y);                           % signal length
 t1 = (0:N-1)/fs;                         % time vector
 
 seizure_begin_h = ceil(seizure_begin/3600);
-seizure_end_h = ceil(seizure_end/3600);
-
 seizure_begin_s = ceil(mod(seizure_begin, 3600));
+seizure_begin_s = seizure_begin_s(seizure_begin_h==str2double(time));
+seizure_end_h = ceil(seizure_end/3600);
 seizure_end_s = ceil(mod(seizure_end, 3600));
-
-if seizure_number ~= -1
-    seizure_number=find(seizure_begin_h == str2double(time));
-end
+seizure_end_s = seizure_end_s(seizure_end_h==str2double(time));
 
 %% fir filter
-for j=[8, 9, 37, 38, 39, 48, 49, 50]
-    figure(j)
-    
-    y=EEG(j,:);
-    k=fs;
-    Nmean = ceil(N / k);
-    t1 = (0:N-1)/fs;                         % time vector
+figure(1)
 
-    Pmean = zeros(1, N);                  
-    for i = 1: N-k 
-        Pmean(i) = mean(y(i: k+i));  % from 1 to k+1 mean, 2 to k+2 mean
-    end
-    
-%     t1 = (0:Nmean-1)/fs;                         % time vector
-%     Pmean = zeros(1, Nmean);                  
-%     for i = 1: Nmean-1
-%         Pmean(i) = mean(y((i-1)*k+1: i*k));  % from 1 to k mean, k+1 to 2k mean
-%     end
+k=fs;
+m=1;
 
-    plot(t1, Pmean, 'r')
-    xlim([0 max(t1)])
-    ylim([-1.1*max(Pmean) 1.1*max(Pmean)])
-    grid on
-    xlabel('Time, s')
-    ylabel('Power, \muW (SOI)')
-    title('FIR');
-
-    if seizure_number ~= -1
-        hold on
-        plot(t1(seizure_begin_s(seizure_number)),0,'b*');
-        plot(t1(seizure_end_s(seizure_number)),0,'c*');
-        hold off
-    end
-% 
-%     hold on
-%     [up, lo] = envelope(Pmean, 20000, 'peak');
-%     plot(t1, up); 
-%     hold off
+Pmean = zeros(1, N);                  
+for i = k:m:N 
+    Pmean(i) = mean(y(i-k+1: i));
 end
 
+y_desired = zeros(1, N/fs);
+
+for i=numel(seizure_begin_s)
+    y_desired(seizure_begin_s(i):seizure_end_s(i))=1;    
+end
+
+plot(t1, Pmean, 'r')
+xlim([0 max(t1)])
+ylim([1.1*min(Pmean) 1.1*max(Pmean)])
+grid on
+xlabel('Time, s')
+ylabel('Power, \muW (SOI)')
+title('FIR');
+hold on 
+stairs((1:3600),y_desired*100, 'b');
+
+index=1;
+factor=1;
+dataset = zeros(N/fs*factor, fs);
+for i=1: fs/factor: N-fs/factor+1 % move every quarter of the second
+    dataset(index,:) = Pmean(i: i+fs-1); % half of the second samples
+    index=index+1;
+end
+writematrix([rescale(dataset, 'InputMax', 100), y_desired'], 'test_data.csv')
--- MATLAB/nn_test.m
+++ MATLAB/nn_test.m
@@ -1,71 +1,71 @@
-T =  readtable(strcat('../neural_network/raw/4_hidden/512_400_300_200_50_1/30000_epochs/train_result.csv'));
-T2 = readtable(strcat('../neural_network/raw/4_hidden/512_400_300_200_50_1/30000_epochs/test_result.csv'));
-
-A = table2array(T);
-A_size = size(A);
-
-subplot(1,2,1);
-stairs((1:3600), A(:, 1), 'r');
-grid on
-xlabel('Time [s]')
-ylabel('Probability')
-xlim([0 3600])
-ylim([0 1.01])
-hold on
-stairs((1:3600), A(:, 2), '--b');
-legend('predicted', 'expected')
-hold off;
-
-A = table2array(T2);
-A_size = size(A);
-
-subplot(1,2,2);
-stairs((1:3600), A(:, 1), 'r');
-grid on
-xlabel('Time [s]')
-ylabel('Probability')
-xlim([0 3600])
-ylim([0 1.01])
-hold on 
-stairs((1:3600), A(:, 2), '--b');
-legend('predicted', 'expected')
-hold off;
-
-% n=3;
-% subplotnum = 1;
-% for i = [1, 7,10]
-%     T =  readtable(strcat('../neural_network/raw/2_hidden/512_400_40_1/',string(i),'000_epochs/train_result.csv'));
-%     T2 = readtable(strcat('../neural_network/raw/2_hidden/512_400_40_1/',string(i),'000_epochs/test_result.csv'));
-%     
-%     A = table2array(T);
-%     A_size = size(A);
+% T =  readtable(strcat('../neural_network/fir/2_hidden/512_400_40_1/1000_epochs/train_result.csv'));
+% T2 = readtable(strcat('../neural_network/fir/2_hidden/512_400_40_1/1000_epochs/test_result.csv'));
 % 
-%     subplot(n,2,2*subplotnum-1);
-%     stairs((1:3600), A(:, 1), 'r');
-%     grid on
-%     xlabel('Time [s]')
-%     ylabel('Probability')
-%     xlim([0 3600])
-%     ylim([0 1.01])
-%     hold on
-%     stairs((1:3600), A(:, 2), '--b');
-%     legend('predicted', 'expected')
-%     hold off;
-%     
-%     A = table2array(T2);
-%     A_size = size(A);
+% A = table2array(T);
+% A_size = size(A);
 % 
-%     subplot(n,2,2*subplotnum);
-%     stairs((1:3600), A(:, 1), 'r');
-%     grid on
-%     xlabel('Time [s]')
-%     ylabel('Probability')
-%     xlim([0 3600])
-%     ylim([0 1.01])
-%     hold on 
-%     stairs((1:3600), A(:, 2), '--b');
-%     legend('predicted', 'expected')
-%     hold off;
-%     
-%     subplotnum = subplotnum + 1;
-% end
+% subplot(1,2,1);
+% stairs((1:3600), A(:, 1), 'r');
+% grid on
+% xlabel('Time [s]')
+% ylabel('Probability')
+% xlim([0 3600])
+% ylim([0 1.01])
+% hold on
+% stairs((1:3600), A(:, 2), '--b');
+% legend('predicted', 'expected')
+% hold off;
+% 
+% A = table2array(T2);
+% A_size = size(A);
+% 
+% subplot(1,2,2);
+% stairs((1:3600), A(:, 1), 'r');
+% grid on
+% xlabel('Time [s]')
+% ylabel('Probability')
+% xlim([0 3600])
+% ylim([0 1.01])
+% hold on 
+% stairs((1:3600), A(:, 2), '--b');
+% legend('predicted', 'expected')
+% hold off;
+
+n=2;
+subplotnum = 1;
+for i = [1, 5]
+    T =  readtable(strcat('../neural_network/fir/2_hidden/512_400_40_1/',string(i),'000_epochs/train_result.csv'));
+    T2 = readtable(strcat('../neural_network/fir/2_hidden/512_400_40_1/',string(i),'000_epochs/test_result.csv'));
+    
+    A = table2array(T);
+    A_size = size(A);
+
+    subplot(n,2,2*subplotnum-1);
+    stairs((1:3600), A(:, 1), 'r');
+    grid on
+    xlabel('Time [s]')
+    ylabel('Probability')
+    xlim([0 3600])
+    ylim([0 1.01])
+    hold on
+    stairs((1:3600), A(:, 2), '--b');
+    legend('predicted', 'expected')
+    hold off;
+    
+    A = table2array(T2);
+    A_size = size(A);
+
+    subplot(n,2,2*subplotnum);
+    stairs((1:3600), A(:, 1), 'r');
+    grid on
+    xlabel('Time [s]')
+    ylabel('Probability')
+    xlim([0 3600])
+    ylim([0 1.01])
+    hold on 
+    stairs((1:3600), A(:, 2), '--b');
+    legend('predicted', 'expected')
+    hold off;
+    
+    subplotnum = subplotnum + 1;
+end
